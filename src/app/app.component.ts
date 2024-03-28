81e4438ad10162cbea44fe071d15d40e4f3b5680
Yacineos <psmartyacine@gmail.com>
2024-03-28 16:31:10 +0100

    path calc not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Signal, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MapOptions, tileLayer, LatLng, LatLngLiteral, marker, icon, Icon } from 'leaflet';
+import { MapOptions, tileLayer, LatLng, LatLngLiteral, marker, icon, Icon, LeafletMouseEvent, Layer, Polyline, polyline, PolylineOptions } from 'leaflet';
+import { OpenRouteService } from './services/open-route.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LeafletModule],
+  imports: [RouterOutlet, LeafletModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  
   readonly defaultIcon = computed( () => icon({
     ...Icon.Default.prototype.options,
     iconUrl: 'assets/marker-icon.png',
@@ -19,21 +23,39 @@ export class AppComponent {
   }) );
   
   readonly center = signal<LatLng>( new LatLng(45.166672, 5.71667) );
-  readonly zoom = signal<number>(12);
+  readonly zoom = signal<number>(11);
 
-  readonly places = signal<readonly LatLngLiteral[]>([
-    { lat: 48.858370, lng: 2.294481 },                   // Tour Eiffel
+  readonly places = signal< LatLngLiteral[]>([
     { lat: 45.193866812447716, lng: 5.768449902534485 }, // UFR IM2AG
-    { lat: 44.56414746666089, lng: 6.495827436447144},   // Place Bartelon
+    { lat: 45.197866812447716, lng: 5.768449902534485 }, // UFR IM2AG
   ]);
+  readonly map = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' });
+  readonly sigPlaces = computed(()=> this.places().map( ({lat, lng}) => marker([lat, lng], {icon: this.defaultIcon()}) ));
+  readonly sigRoutes = computed<Polyline[]>(()=> [new Polyline(this.places() as LatLng[], {color: '#0d9148'} as PolylineOptions)]);
   
-  readonly options: Signal<MapOptions> = computed(() => ({
-    layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      ...this.places().map( ({lat, lng}) => marker([lat, lng], {icon: this.defaultIcon()}) )
+  readonly layers: Signal<Layer[]> = computed(() => (
+    [
+      this.map ,
+      ...this.sigPlaces(),
+      ...this.sigRoutes(),
     ]
-  }) )
-  
+  ));
+  // polyline 
+  // circle / zone 
+  // isocrone c'est pour voir à partir de coordonnées , combien de temps on met avec un vélo , a pied , transport
+
+
+  // le track by il assure que quand le css il va appliquer une transition à un fragement HTml , 
+  // si le fragement il est fraichement crée -> pas d'état précédent -> il applique pas la transition
+
+  constructor(private service: OpenRouteService){
+
+  }
+
+  addMarker(point : LeafletMouseEvent){
+    this.places.update((places) => [...places,point.latlng]);
+  }
+
   allerVoirEmbrun(): void {
     this.center.set(new LatLng(44.566672, 6.5));
     this.zoom.set(13);
@@ -47,6 +69,18 @@ export class AppComponent {
   allerVoirUFR(): void {
     this.center.set(new LatLng(45.19379120519956, 5.768213868141175));
     this.zoom.set(18);
+  }
+
+  removeButton(point: LatLngLiteral){
+    this.places.update((points) => points.filter((pnt) => pnt !== point));
+  }
+
+  async addRoute(){
+    const newLayerEncoded = await firstValueFrom(this.service.getRoute(this.places()));
+    // const poly = require("@mapbox/polyline");
+    // const layer = poly.decode(newLayerEncoded.routes[0].geometry);
+    // console.log
+    // this.sigRoutes.update( (routes) => [...routes,newLayer]);
   }
 
 }
